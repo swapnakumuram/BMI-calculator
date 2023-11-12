273faa77249586944f82f43d749f0a366fe5f5e0
swapnakumuram <121389837+swapnakumuram@users.noreply.github.com>
2023-11-12 20:44:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import "./App.css";
+import React,{useState} from "react";
+
+ function App() {
+   const [weight,setWeight]= useState(0);
+   const [height, setHeight] = useState(0);
+   const [bmi, setBmi] = useState('');
+   const [message, setMessage] = useState('');
+
+  let calcBmi = (e) => {
+    e.preventDefault();    //edi rayaka pothe sec result vellipothundi
+    if(weight===0 || height===0)
+    {
+      alert ('please enter a valid weight and height')
+    }
+    else {
+    let bmi = (weight/(height*height)*703)
+  setBmi(bmi.toFixed(1))
+
+
+  if(bmi<25) {
+    setMessage('you are underweight')
+  }
+  else if (bmi>=25 && bmi <30 ){
+    setMessage('you are a healthy weight')
+  }
+  else{
+    setMessage('you are overweight')
+  }
+  }
+}
+
+ let reload = () => {
+  window.location.reload()
+}
+
+
+
+  return (
+    <div className="App">
+      <div className="container">
+      <h2>BMI Calculator</h2>
+      <form onSubmit={calcBmi}>
+        <div>
+          <label>Weight (ibs)</label>
+          <input 
+          type="text" 
+          placeholder="Enter weight value"
+           value={weight}
+           onChange={(e) => setWeight(e.target.value)} />
+        </div>
+        <div>
+          <label>Height (in)</label>
+          <input type="text" placeholder="Enter height value" 
+          value={height}
+          onChange={(event) => setHeight(event.target.value)}
+           />
+        </div>
+        <div>
+          <button className="btn" type="submit">Submit</button>
+          <button className="btn btn-outline" onClick={reload} type="submit">Reload</button>
+        </div>
+
+        <div className="center">
+          <h3>Your BMI is:{bmi}</h3>
+          <p>{message}</p>
+        </div>
+      </form>
+      </div>
+    </div>
+  );
+}
+export default App;
